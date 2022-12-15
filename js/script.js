aa5714aad2fa4baa16996fb01e32d9030ed2ebc8
Dialcab <66257079+Dialcab@users.noreply.github.com>
2022-12-16 06:47:47 +0800

    Semi-finished
    Design finished but no animations

--- js/script.js
+++ js/script.js
@@ -1,111 +1,19 @@
-/* To Top Button */
-
-let mybutton = document.getElementById("ToTopBtn");
-
-window.onscroll = function() {
-    scrollFunction()
-};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-    mybutton.style.display = "none";
-    }
-}
-
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
-
-/* Stcky Navigation bar */
-
-window.addEventListener("scroll", function() {
-    var header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
-});
-
-/* Logo Swap on Scroll */
-
-var logoWhite = "images/logoWhite.png";
-var logoBlack = "images/logoBlack.png";
-
- window.addEventListener("scroll", function() {
-    if(window.scrollY > 0) {
-        document.getElementById("logo").src=logoBlack;
-    } else {
-        document.getElementById("logo").src=logoWhite;
-    }
- });
-
- /* Smooth Mouse Scrolling */
-
- function init(){
-	new SmoothScroll(document,120,12)
-}
-
-function SmoothScroll(target, speed, smooth) {
-	if (target === document)
-		target = (document.scrollingElement 
-              || document.documentElement 
-              || document.body.parentNode 
-              || document.body) // cross browser support for document scrolling
-      
-	var moving = false
-	var pos = target.scrollTop
-  var frame = target === document.body 
-              && document.documentElement 
-              ? document.documentElement 
-              : target // safari is the new IE
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+  }
   
-	target.addEventListener('mousewheel', scrolled, { passive: false })
-	target.addEventListener('DOMMouseScroll', scrolled, { passive: false })
-
-	function scrolled(e) {
-		e.preventDefault(); // disable default scrolling
-
-		var delta = normalizeWheelDelta(e)
-
-		pos += -delta * speed
-		pos = Math.max(0, Math.min(pos, target.scrollHeight - frame.clientHeight)) // limit scrolling
-
-		if (!moving) update()
-	}
-
-	function normalizeWheelDelta(e){
-		if(e.detail){
-			if(e.wheelDelta)
-				return e.wheelDelta/e.detail/40 * (e.detail>0 ? 1 : -1) // Opera
-			else
-				return -e.detail/3 // Firefox
-		}else
-			return e.wheelDelta/120 // IE,Safari,Chrome
-	}
-
-	function update() {
-		moving = true
-    
-		var delta = (pos - target.scrollTop) / smooth
-    
-		target.scrollTop += delta
-    
-		if (Math.abs(delta) > 0.5)
-			requestFrame(update)
-		else
-			moving = false
-	}
-
-	var requestFrame = function() { // requestAnimationFrame cross browser
-		return (
-			window.requestAnimationFrame ||
-			window.webkitRequestAnimationFrame ||
-			window.mozRequestAnimationFrame ||
-			window.oRequestAnimationFrame ||
-			window.msRequestAnimationFrame ||
-			function(func) {
-				window.setTimeout(func, 1000 / 50);
-			}
-		);
-	}()
-}
+  function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("service-preview");
+    var dots = document.getElementsByClassName("preview-card");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" highlight-on", "");
+    }
+    x[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " highlight-on";
+  }

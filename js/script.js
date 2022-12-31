26396dab498e5891d71a7b36d39a3a81f6a733ed
Dialcab <66257079+Dialcab@users.noreply.github.com>
2022-12-31 13:47:55 +0800

    New Design Semi-Complete
    No Content in Projects

--- js/script.js
+++ js/script.js
@@ -1,19 +1,41 @@
-function currentDiv(n) {
-    showDivs(slideIndex = n);
+filterSelection("graphic")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("service-item");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
   }
-  
-  function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("service-preview");
-    var dots = document.getElementsByClassName("preview-card");
-    if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length}
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" highlight-on", "");
-    }
-    x[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " highlight-on";
+}
+
+function AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
   }
+}
+
+function RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+var btnContainer = document.getElementById("service-filter");
+var btns = btnContainer.getElementsByClassName("service-selection");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
